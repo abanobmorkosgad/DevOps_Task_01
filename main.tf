7cc255576c4a8f936ab135df17c22a5d59877515
Abanob Morkos <abanobmorkos13@gmail.com>
2024-06-30 10:56:26 +0300

    automatic inventory

--- main.tf
+++ main.tf
@@ -109,3 +109,7 @@ resource "aws_eip_association" "eip_association" {
   instance_id   = aws_instance.ec2.id
   allocation_id = aws_eip.eip.id
 }
+
+output "elastic_ip" {
+  value = aws_eip.eip.public_ip
+}
